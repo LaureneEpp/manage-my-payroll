ed6f2662760ae7273eb573e059c7fdfdc739d5ea
LaureneEpp <laurene.eppelle@gmail.com>
2023-05-17 13:58:28 +0200

    refactoring-sideMenu

--- app/javascript/custom/sideMenu.js
+++ app/javascript/custom/sideMenu.js
@@ -8,22 +8,29 @@ const subMenu = document.querySelector("#submenu")
 var w = document.documentElement.clientWidth || window.innerWidth;
 
 
-close_icon.addEventListener('click', openSidebar);
-menu_icon.addEventListener('click', openSidebar);
-arrow.addEventListener('click', dropdown)
-
 function dropdown() {
-    subMenu.classList.toggle("hidden");
-    arrow.classList.toggle("rotate-0");
+  subMenu.classList.toggle("hidden");
+  arrow.classList.toggle("rotate-0");
   }
   dropdown();
 
 function openSidebar() {
   sideMenu.classList.toggle("hidden");
-  if  ((!sideMenu.classList.contains('hidden')) && w >= 1023 && w <= 768) {
+  if  ((!sideMenu.classList.contains('hidden')) && (w >= 1023)){
     main_container.style.marginLeft = "25%"
-    sideMenu.style.width = "25%"
-    console.log('Test for Sidebar')
+    sideMenu.classList.add("w-1/4")
+    sideMenu.classList.add("block")
+    console.log('Test01 for Sidebar')
+  }
+  if  (w <= 768){
+    main_container.style.removeProperty("marginLeft");
+    sideMenu.classList.remove("w-1/4")
+    sideMenu.classList.remove("block")
+    console.log('Test02 for Sidebar')
   }
 }
 openSidebar();
+  
+close_icon.addEventListener('click', openSidebar);
+menu_icon.addEventListener('click', openSidebar);
+arrow.addEventListener('click', dropdown)
