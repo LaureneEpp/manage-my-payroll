bd843587a714bda5ee3ac35978e64f479bd68259
LaureneEpp <laurene.eppelle@gmail.com>
2023-05-17 14:40:29 +0200

    create-dashboard-root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'employees#index' # shortcut for the abov
+  root 'pages#dashboard' # shortcut for the abov
   resources :employees
 end
