9498c3d4a9f211d904c7eb8165c64301e218c2a4
LaureneEpp <laurene.eppelle@gmail.com>
2023-06-14 13:48:54 +0200

    Refactoring modal on index for taxation indexes"

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -1,26 +1,42 @@
-// const modals = document.querySelectorAll('#modal-id')
-// const backdrops = document.querySelectorAll('#modal-id-backdrop')
-// const btns = document.querySelectorAll('.btn-allowance')
+// get all links
+const openModalButtons = document.querySelectorAll('.taxation-button');
+const closeModalButtons = document.querySelectorAll('.closeModalButton');
+const modals = document.querySelectorAll('.taxation-modal');
+const overlay = document.querySelector('#overlay');
 
-// function toggleModal() {
-//     modals.forEach(modal => {
-//         modal.classList.toggle("hidden");
-//         modal.classList.toggle("flex");
-//         console.log("test modal01")
+// Open modal : visible, 
+function openModal() {
+    modals.forEach(modal => {
+        modal.classList.remove('hidden');
+        modal.classList.add('z-40')
+        overlay.classList.remove('hidden');
+        document.body.classList.add('overflow-hidden');
+        document.addEventListener('keydown', handleKeyDown);
+    })
+    console.log('Test for modal on open button');
+}
 
-//     })
-//     backdrops.forEach(backdrop => {
-//         backdrop.classList.toggle("hidden");
-//         backdrop.classList.toggle("flex");
-//         console.log("test modal02")
+// Close modal : not visible, bg-opacity 
 
+function closeModal() {
+    modals.forEach(modal => {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+        document.body.classList.remove('overflow-hidden');
+        document.removeEventListener('keydown', handleKeyDown);      
+    })
+    console.log('Test for modal on closed button');
+}
 
-//     })
-//     console.log("test modal")
-// }
+// escape when click on X/close
 
-      
-// btns.forEach(btn => {
-//     btn.addEventListener('click', toggleModal);
-// })
+function handleKeyDown(event) {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+}
+  
+openModalButtons.forEach(open => open.addEventListener('click', openModal))
+
+closeModalButtons.forEach(close => close.addEventListener('click', closeModal))
 
