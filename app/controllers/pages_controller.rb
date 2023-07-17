409a4ec55062316f0b35ffac6c28f252386b48f9
LaureneEpp <laurene.eppelle@gmail.com>
2023-07-17 20:17:59 +0200

    add section for action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
     @employees = Employee.all.order('last_name ASC')
     @managers = Employee.where(manager: true).order('last_name ASC')
     @employee = Employee.new
+    @tbd_employees = Employee.joins(:position, :team).where("positions.name = 'TBD' OR teams.name = 'TBD'")
   end
 end
