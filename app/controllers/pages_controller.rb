b339b3f94c63e9396b3a4d1401445d12050c4a07
LaureneEpp <laurene.eppelle@gmail.com>
2023-07-21 00:30:32 +0200

    create search engine with Ransack gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   def dashboard
     @employees_count = Employee.count
-    @employees = Employee.all.order('last_name ASC')
+    # @tbd_position = Employee.where(position: 'TBD')
+    # @tbd_team = Employee.where(team: 'TBD')
+    # @tbd_dpt = Team.joins(:departement).where(departements: {name: 'TBD'})
+    @employees = Employee.all.order('last_name ASC').excluding(@tbd_employees)
     @managers = Employee.where(manager: true).order('last_name ASC')
     @employee = Employee.new
     @tbd_employees = Employee.joins(:position, :team).where("positions.name = 'TBD' OR teams.name = 'TBD'")
