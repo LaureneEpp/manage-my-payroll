ed8ec8f891c9288228c1e74bd3acceb79e80f564
LaureneEpp <laurene.eppelle@gmail.com>
2023-07-29 18:21:20 +0200

    config

--- db/migrate/20230515140659_create_employees.rb
+++ db/migrate/20230515140659_create_employees.rb
@@ -6,7 +6,7 @@ class CreateEmployees < ActiveRecord::Migration[7.0]
       t.string :email
       t.string :city
       t.string :country
-      t.boolean :manager
+      t.boolean :manager, default: false
       t.references :team, null: false, foreign_key: true
       t.references :position, null: false, foreign_key: true
 
